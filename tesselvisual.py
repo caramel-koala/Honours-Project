d4908bf108551582690f6a4b1e8f11993acf2279
caramel-koala <antoniob.peters@gmail.com>
2016-04-21 21:59:16 +0200

    First Commit

---
+++ tesselvisual.py
@@ -0,0 +1,14 @@
+def tesselvisual(arr_tessel, arr_points):
+
+    #overhead
+    import numpy as np
+    from scipy.spatial import Voronoi, voronoi_plot_2d
+    import shapely.geometry
+    import shapely.ops
+    #Set up canvas
+
+    #display points
+
+    #display tesselations
+
+    return
